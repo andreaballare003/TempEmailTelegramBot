932f881240566839ce8cfe25805f66e0f627e221
andrea <ballareandrea09@gmail.com>
2025-04-11 19:05:18 +0200

    Inizio bot

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+import org.telegram.telegrambots.longpolling.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
